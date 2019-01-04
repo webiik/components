8b6d588cf978c9693cea246687be9bf0217c6df7
Jiri Mihal <jiri@mihal.me>
2019-01-04 22:34:48 +0100

    info Under development

--- src/Webiik/Log/Log.php
+++ src/Webiik/Log/Log.php
@@ -18,6 +18,13 @@ class Log
     const DEBUG = 'debug';
 
     /**
+     * In silent mode unavailable loggers not throw exceptions
+     * instead of it these exceptions are logged with available loggers.
+     * @var bool
+     */
+    private $silent = false;
+
+    /**
      * @var Container
      */
     private $container;
@@ -43,25 +50,29 @@ class Log
     }
 
     /**
-     * Logs with an arbitrary level in PSR-3 format
-     * @link https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md#5-psrlogloglevel
-     *
-     * @param string $level PSR-3 log level
+     * @param bool $silent
+     */
+    public function setSilent(bool $silent): void
+    {
+        $this->silent = $silent;
+    }
+
+
+    /**
+     * Todo: Implement groups
      * @param string $message
      * @param array $context
      * @param array $data
+     * @param string $group
      */
-    public function log(string $level, string $message, array $context = [], array $data = []): void
+    public function info(string $message, array $context = [], array $data = [], string $group = ''): void
     {
-        if ($context) {
-            $message = $this->parseContext($message, $context);
-        }
-
-        $this->records[] = new Record($level, $message, $data);
     }
 
+    // Todo: Add another PSR-3 log level methods
+
     /**
-     * Add logger to appropriate log level(s)
+     * Add logger with appropriate log level(s)
      * @param string $logger
      * @param array $levels
      */
@@ -80,32 +91,117 @@ class Log
             ];
         }
 
-        $this->loggers[] = [$logger, $levels];
+        $this->loggers[$logger] = $levels;
     }
 
     /**
+     * Logs with an arbitrary level in PSR-3 format
+     * @link https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md#5-psrlogloglevel
+     *
+     * @param string $level PSR-3 log level
+     * @param string $message
+     * @param array $context
+     * @param array $data
+     */
+    private function log(string $level, string $message, array $context = [], array $data = []): void
+    {
+        if ($context) {
+            $message = $this->parseContext($message, $context);
+        }
+
+        $this->records[] = new Record($level, $message, $data);
+    }
+
+    /**
+     * Remove logger
+     * @param string $logger
+     */
+    private function delLogger(string $logger): void
+    {
+        unset($this->loggers[$logger]);
+    }
+
+
+    /**
      * Write log records using the registered loggers and remove all records
+     * @throws \Exception
      */
     public function write(): void
     {
-        foreach ($this->loggers as $arr) {
+        $catchedExceptions = [];
+
+        foreach ($this->loggers as $loggerName => $loggerLogLevels) {
+            // Prepare only records associated with log level(s) of iterated logger
             $loggerRecords = [];
             foreach ($this->records as $record) {
                 /* @var $record Record */
                 $recordLogLevel = $record->getLevel();
-                foreach ($arr[1] as $loggerLoglevel) {
-                    if ($loggerLoglevel == $recordLogLevel) {
+                foreach ($loggerLogLevels as $loggerLogLevel) {
+                    if ($loggerLogLevel == $recordLogLevel) {
                         $loggerRecords[] = $record;
                     }
                 }
             }
 
-            /** @var LoggerInterface $logger */
-            $logger = $this->container->get($arr[0]);
-            $logger->process($loggerRecords);
+            // Process records
+            if ($loggerRecords) {
+                try {
+                    /** @var LoggerInterface $logger */
+                    $logger = $this->container->get($loggerName);
+                    $logger->process($loggerRecords);
+                } catch (\Exception $exception) {
+                    // When logger doesn't exist remove it from loggers and log exception.
+                    // This is necessary for safe and uninterrupted logging.
+                    $this->delLogger($loggerName);
+                    $catchedExceptions[] = $exception;
+                } catch (\TypeError $exception) {
+                    $this->delLogger($loggerName);
+                    $catchedExceptions[] = $exception;
+                }
+            }
         }
 
+        // Clear all written records
         $this->records = [];
+
+        if ($this->silent) {
+            // Log exceptions about non existing loggers
+            $this->logExceptions($catchedExceptions);
+        } else {
+            // Throw exception from first non existing logger
+            $this->throwExceptions($catchedExceptions);
+        }
+    }
+
+    /**
+     * Log exceptions in Exceptions array
+     * @param array $exceptions
+     * @throws \Exception
+     */
+    private function logExceptions(array $exceptions): void
+    {
+        if ($exceptions) {
+            foreach ($exceptions as $exception) {
+                /** @var \Exception $exception */
+                $msg = '[Exception] ' . $exception->getMessage() . ' in ' . $exception->getFile();
+                $msg .= ' on line ' . $exception->getLine();
+                $this->log(self::WARNING, $msg, [], $exception->getTrace());
+            }
+            $this->write();
+        }
+    }
+
+    /**
+     * Throw first exceptions in Exceptions array
+     * @param array $exceptions
+     * @throws \Exception
+     */
+    private function throwExceptions(array $exceptions): void
+    {
+        foreach ($exceptions as $exception) {
+            /** @var \Exception $exception */
+            throw $exception;
+        }
     }
 
     /**
