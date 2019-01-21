2df83ec91bda2588cb8ae7e6c88ac5e1b8aa99c2
Jiri Mihal <jiri@mihal.me>
2019-01-21 21:02:31 +0100

    add Method isIn

--- Arr.php
+++ Arr.php
@@ -6,7 +6,7 @@ namespace Webiik\Arr;
 class Arr
 {
     /**
-     * Set an item into array using dot notation
+     * Set the item into the array using the dot notation
      * @param string $key
      * @param mixed $val
      * @param array $array
@@ -23,7 +23,7 @@ class Arr
     }
 
     /**
-     * Add an item into array using dot notation
+     * Add the item into the array using the dot notation
      * @param string $key
      * @param mixed $val
      * @param array $array
@@ -53,10 +53,10 @@ class Arr
     }
 
     /**
-     * Get an item from array using dot notation
+     * Get the item from the array using the dot notation
      * @param array $array
      * @param string $key
-     * @return mixed|bool
+     * @return mixed
      */
     public function get(string $key, array $array)
     {
@@ -70,12 +70,34 @@ class Arr
     }
 
     /**
-     * Delete an item from array using dot notation
-     * On success return key value, otherwise false
+     * Check if the item is in the array using the dot notation
+     * @param array $array
+     * @param string $key
+     * @return bool
+     */
+    public function isIn(string $key, array $array): bool
+    {
+        $isIn = false;
+        $keys = explode('.', $key);
+
+        foreach ($keys as $ikey) {
+            if (!isset($array[$ikey])) {
+                $isIn = false;
+                break;
+            }
+            $array = $array[$ikey];
+            $isIn = true;
+        }
+
+        return $isIn;
+    }
+
+    /**
+     * Delete the item from the array using the dot notation
      * @param string $key
      * @param array $array
      */
-    public function delete(string $key, array &$array)
+    public function delete(string $key, array &$array): void
     {
         $keys = explode('.', $key);
         $context = &$array;
