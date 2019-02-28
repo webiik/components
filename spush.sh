ea4c62946a19506755a1f0d0d3c7822567c0f207
Jiri Mihal <jiri@mihal.me>
2019-02-28 22:30:21 +0100

    add Subtree bulk push script

---
+++ spush.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+git subtree push --prefix=src/Webiik/App App master --squash
+git subtree push --prefix=src/Webiik/Arr Arr master --squash
+git subtree push --prefix=src/Webiik/Container Container master --squash
+git subtree push --prefix=src/Webiik/Cookie Cookie master --squash
+git subtree push --prefix=src/Webiik/Csrf Csrf master --squash
+git subtree push --prefix=src/Webiik/CurlHttpClient CurlHttpClient master --squash
+git subtree push --prefix=src/Webiik/Data Data master --squash
+git subtree push --prefix=src/Webiik/Database Database master --squash
+git subtree push --prefix=src/Webiik/Error Error master --squash
+git subtree push --prefix=src/Webiik/Flash Flash master --squash
+git subtree push --prefix=src/Webiik/Log Log master --squash
+git subtree push --prefix=src/Webiik/Login Login master --squash
+git subtree push --prefix=src/Webiik/Mail Mail master --squash
+git subtree push --prefix=src/Webiik/Middleware Middleware master --squash
+git subtree push --prefix=src/Webiik/OAuth1Client OAuth1Client master --squash
+git subtree push --prefix=src/Webiik/OAuth2Client OAuth2Client master --squash
+git subtree push --prefix=src/Webiik/Router Router master --squash
+git subtree push --prefix=src/Webiik/Session Session master --squash
+git subtree push --prefix=src/Webiik/Token Token master --squash
+git subtree push --prefix=src/Webiik/Translation Translation master --squash
+git subtree push --prefix=src/Webiik/Validator Validator master --squash
+git subtree push --prefix=src/Webiik/View View master --squash
