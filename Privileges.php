7d953b43e33c96cca4e159408798c977653312a0
Jiri Mihal <jiri@mihal.me>
2019-03-28 18:03:01 +0100

    add New components

---
+++ Privileges.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Webiik\Account;
+
+class Account
+{
+    public function signup(array $credentials, string $role = ''): bool
+    {
+
+    }
+
+    public function isSignedUp(array $credentials): array
+    {
+
+    }
+
+
+}
