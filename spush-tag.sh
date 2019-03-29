b9d15ed8ddb70751e6fa7edbaae5abe26e0d3d89
Jiri Mihal <jiri@mihal.me>
2019-03-29 15:00:01 +0100

    first commit

---
+++ spush-tag.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Gather required parameters from user
+if [[ -z "$1" ]]; then
+	echo -e "ERR: Missing required parameters"
+	echo -e "Usage: spush-tag [tag] [repo-dir(s)]"
+	exit
+fi
+
+dirs=("$@")
+currentDir=${PWD}
+
+# Remove repos
+if [[ ! -d ~/webiik-repos/ ]]; then
+	mkdir ~/webiik-repos/
+fi
+
+if [[ "$#" == 1 ]]; then
+	# Tag all repos
+
+	# List all directories in src/Webiik
+	for dir in src/Webiik/*
+	do
+		# Remove existing split repo dir
+		if [[ -d ~/webiik-repos/${dir} ]]; then
+			sudo rm -r ~/webiik-repos/${dir}
+		fi
+
+		# Remove the trailing "/"
+		dir=${dir%*/}
+
+		# Get everything after the final "/"
+		dir=${dir##*/}
+
+		mkdir ~/webiik-repos/${dir}
+
+		cd ~/webiik-repos/${dir}
+
+		git init --bare
+
+		cd ${currentDir}
+
+		git push ~/webiik-repos/${dir} ${dir}:master
+
+		cd ~/webiik-repos/${dir}
+
+		repo=$(echo ${dir} | tr '[:upper:]' '[:lower:]')
+
+		git remote add origin https://github.com/webiik/${repo}.git
+
+		git tag ${1}
+
+		git push origin master --tags --force
+
+		cd ${currentDir}
+
+		git subtree push --prefix=src/Webiik/${dir} ${dir} master --squash
+	done
+fi
+
+if [[ "$#" > 1 ]]; then
+	# Tag one or more repos
+	for dir in "${dirs[@]:1}"
+	do
+		# Check if dir exists
+		if [[ ! -d "src/Webiik/${dir}" ]]; then
+			echo -e "🚨ERR: src/Webiik/${dir} doesn't exist, skipped."
+			continue
+		fi
+
+		# Remove existing split repo dir
+		if [[ -d ~/webiik-repos/${dir} ]]; then
+			sudo rm -r ~/webiik-repos/${dir}
+		fi
+
+		mkdir ~/webiik-repos/${dir}
+
+		cd ~/webiik-repos/${dir}
+
+		git init --bare
+
+		cd ${currentDir}
+
+		git push ~/webiik-repos/${dir} ${dir}:master
+
+		cd ~/webiik-repos/${dir}
+
+		repo=$(echo ${dir} | tr '[:upper:]' '[:lower:]')
+
+		git remote add origin https://github.com/webiik/${repo}.git
+
+		git tag ${1}
+
+		git push origin master --tags --force
+
+		cd ${currentDir}
+
+		git subtree push --prefix=src/Webiik/${dir} ${dir} master --squash
+	done
+fi
