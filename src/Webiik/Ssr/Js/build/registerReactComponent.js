5ffa2f436f5938dacb309df3ce56e975846480ae
Jiri Mihal <jiri@mihal.me>
2021-07-02 15:08:16 +0200

    update Compile to ES5

--- src/Webiik/Ssr/Js/build/registerReactComponent.js
+++ src/Webiik/Ssr/Js/build/registerReactComponent.js
@@ -1,24 +1,35 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import { renderToString } from 'react-dom/server';
 import * as ReactDOM from 'react-dom';
 import React from "react";
 window.WebiikReact = {};
 function registerReactComponent(components) {
-    Object.keys(components).forEach(name => {
-        const Component = components[name];
-        window.WebiikReact[name] = (server, uid, props) => {
+    Object.keys(components).forEach(function (name) {
+        var Component = components[name];
+        window.WebiikReact[name] = function (server, uid, props) {
             if (server == 'server') {
-                let html = '<div id="' + uid + '">';
-                html += renderToString(React.createElement(Component, Object.assign({}, props)));
+                var html = '<div id="' + uid + '">';
+                html += renderToString(React.createElement(Component, __assign({}, props)));
                 html += '</div>';
                 return html;
             }
             if (server == 'server-client') {
-                const el = document.getElementById(uid);
-                ReactDOM.hydrate(React.createElement(Component, Object.assign({}, props)), el);
+                var el = document.getElementById(uid);
+                ReactDOM.hydrate(React.createElement(Component, __assign({}, props)), el);
             }
             if (server == 'client') {
-                const el = document.getElementById(uid);
-                ReactDOM.render(React.createElement(Component, Object.assign({}, props)), el);
+                var el = document.getElementById(uid);
+                ReactDOM.render(React.createElement(Component, __assign({}, props)), el);
             }
         };
     });
